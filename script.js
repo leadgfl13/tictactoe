f92cbf69aba36b77935c9bd806331aa8fdebf7fd
leadgfl13 <leadgfl13@gmail.com>
2023-08-09 21:45:19 -0400

    Add script file

---
+++ script.js
@@ -0,0 +1,23 @@
+let cat = () =>{
+    alert("Hello")
+}
+cat()
+
+
+function cat2(){
+    alert("hello")
+}
+
+cat2()
+
+let cat3 = function(){
+    alert("hello")
+}
+
+cat3()
+
+(function(){
+    alert("hello")}
+())
+
+(function sayName(name) { console.log(name) } )('Millzie')
