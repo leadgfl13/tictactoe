a586efe01e8478e05e8b30e0137dd0d10df9268c
leadgfl13 <leadgfl13@gmail.com>
2023-08-17 12:38:30 -0400

    Create IIFE that renders gameboard

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 // set array equal to gameboard object
 //this should be a module
 let gameBoard = (() =>{
+
+    const gameboardArray = []
     let gamearea = document.getElementById('gamearea')
-    let square = document.createElement('div')
-    square.setAttribute('id', 'square')
-    gamearea.append(square)
+    for (let i =0; i<9; i++){
+        const square = document.createElement('div')
+        square.setAttribute('id', 'square')
+        square.innerHTML= 'X'
+        gameboardArray.push(square)
+        console.log(gameboardArray.length)
+        for (squares in gameboardArray){
+            gamearea.append(square)
+        }
+    }
+ 
 })()
+
+
