5654dc6deb994cbae26759f3890dfd7e0ef71d1e
leadgfl13 <leadgfl13@gmail.com>
2023-08-23 16:48:46 -0400

    Refactor code.  Make player factory, make 2d array, make function that checks current player, switches it, and puts that player token on button when pressed

--- script.js
+++ script.js
@@ -1,20 +1,50 @@
-// set array equal to gameboard object
-//this should be a module
-let gameBoard = (() =>{
+const playerFactory = (name, token) =>{
+    return{name, token}
 
-    const gameboardArray = []
-    let gamearea = document.getElementById('gamearea')
-    for (let i =0; i<9; i++){
-        const square = document.createElement('div')
-        square.setAttribute('id', 'square')
-        square.innerHTML= 'X'
-        gameboardArray.push(square)
-        console.log(gameboardArray.length)
-        for (squares in gameboardArray){
-            gamearea.append(square)
+}
+
+const playerOne = playerFactory("p1", 'O')
+const playerTwo = playerFactory('p2', 'X')
+
+
+
+
+const gameBoard = (()=>{
+
+    const getcurrentPlayer = () =>{
+        if(currentPlayer == playerOne){
+            currentPlayer = playerTwo
+        }
+      else  if(currentPlayer ==playerTwo){
+            currentPlayer = playerOne
         }
     }
- 
-})()
+    const gameArray= []
+    let currentPlayer = playerOne
+    const gameArea = document.getElementById('gamearea')
+    for(let i = 0; i<9; i++){
+        const square = document.createElement('button')
+        square.setAttribute('id', 'square')
+        square.addEventListener('click', ()=>{
+            getcurrentPlayer()
+            square.innerHTML = currentPlayer.token
+            console.log(currentPlayer)
+        })
+        gameArray.push(square)
+        gameArea.append(gameArray[i])
+        }
+        
+     // const makeMark = (player,token) =>{
+        //console.log
+
+      //}
+     
+    })();
+
+
+
+
+
+
 
 
